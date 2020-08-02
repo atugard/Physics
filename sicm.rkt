e60d3feef71f12638bcfd83a6e88d4aa850ce3c4
David Draguta <daviddraguta@gmail.com>
2020-08-02 00:54:04 -0400

    Implement install-tuples-package, which contains implementations for most tuple functionality described in the appendix of sicm.

--- sicm.rkt
+++ sicm.rkt
@@ -14,65 +14,130 @@
       (car l)
       null))
 (define (tagged-list? l)
-  (symbol? (car l)))
-(define (up . args)
+  (if (pair? l)
+      (symbol? (car l))
+      false))
+
+(define (install-tuples-package m)
+  (define (up . args)
   (tag args 'up))
-(define (down . args)
-  (tag args 'down))
-(define (ref tup i)
-  (let ((tup (cdr tup)))
-    (define (traverse tup i)
-      (cond [(null? tup)
-             null]
-            [(eq? i 0)
-             (car tup)]
-            [else
-             (traverse (cdr tup) (-- i))]))
-    (traverse tup i)))
+  (define (down . args)
+    (tag args 'down))
+  (define (ref tup i)
+    (let ((tup (cdr tup)))
+      (define (traverse tup i)
+        (cond [(null? tup)
+               null]
+              [(eq? i 0)
+               (car tup)]
+              [else
+               (traverse (cdr tup) (-- i))]))
+      (traverse tup i)))
+  (define ((component . args) tup)
+    (if (null? args)
+        tup
+        ((apply component (cdr args)) (ref tup (car args)))))
+  (define (+tup val1 val2)
+        (if (and (tagged-list? val1) (tagged-list? val2)
+             (and (eq? (length val1) (length val2))))
+            (let ((tag1 (get-tag val1))
+                  (tag2 (get-tag val2))
+                  (rest1 (cdr val1))
+                  (rest2 (cdr val2)))
+              (define (add l1 l2)
+                (if (null? l1)
+                    null
+                    (cons (list '+ (car l1) (car l2)) (add (cdr l1) (cdr l2)))))
+              (cond [(and (eq? tag1 'up) (eq? tag2 'up))
+                     (apply up (add rest1 rest2))]
+                    [(and (eq? tag1 'down) (eq? tag2 'down))
+                     (apply down (add rest1 rest2))]
+                    [else
+                     (error "This operation requires either up, up or down, down, you gave: " tag1 tag2)]))
+        (error "This operation requires that both arguments be tagged tuples of the same length.")))
+  (define (-tup val1 val2)
+    (if (and (tagged-list? val1) (tagged-list? val2)
+             (and (eq? (length val1) (length val2))))
+        (let ((tag1 (get-tag val1))
+              (tag2 (get-tag val2))
+              (rest1 (cdr val1))
+              (rest2 (cdr val2)))
+          (define (sub l1 l2)
+            (if (null? l1)
+                null
+                (cons (list '- (car l1) (car l2)) (sub (cdr l1) (cdr l2)))))
+          (cond [(and (eq? tag1 'up) (eq? tag2 'up))
+                 (apply up (sub rest1 rest2))]
+                [(and (eq? tag1 'down) (eq? tag2 'down))
+                 (apply down (sub rest1 rest2))]
+                [else
+                 (error "This operation requires either up, up or down, down, you gave: " tag1 tag2)]))
+        (error "This operation requires that both tuples be tagged up or down, and be of the same length.")))
+  (define (*tup val1 val2)
+    (if (and (tagged-list? val1) (tagged-list? val2)
+             (and (eq? (length val1) (length val2))))
+        (let ((tag1 (get-tag val1))
+              (tag2 (get-tag val2)))
+          (cond [(or (and (eq? tag1 'down) (eq? tag2 'up))
+                     (and (eq? tag1 'up) (eq? tag2 'down)))
+                 (define (dot-prod l1 l2)
+                   (define (componentwise* l1 l2)
+                     (if (null? l1)
+                         null
+                         (cons (list '* (car l1) (car l2)) (componentwise* (cdr l1) (cdr l2)))))
+                   (cons '+ (componentwise* l1 l2)))
+                 (dot-prod (cdr val1) (cdr val2))]
+                [else (error "This operation requires either up, down or down, up, you gave: " tag1 tag2)]))
+        (error "This operation requires two tuples of the same length.")))
+  (define (a*tup val1 val2)
+    (if (and (number? val1) (tagged-list? val2))
+        (let ((tag2 (get-tag val2)))
+          (define (scalar-mul l)
+            (if (null? l)
+                null
+                (cons (list '* val1 (car l)) (scalar-mul (cdr l)))))
+          (cond [(eq? 'up tag2)
+                 (apply up (scalar-mul (cdr val2)))]
+                [(eq? 'down tag2)
+                 (apply down (scalar-mul (cdr val2)))]
+                [else
+                 (error "Unrecognized tag: " tag2)]))
+        (error "We require two arguments: the first a number, the second a tuple either tagged up or down.")))
+  (define (dispatch m)
+    (cond [(eq? m 'up) up]
+          [(eq? m 'down) down]
+          [(eq? m 'ref) ref]
+          [(eq? m 'component) component]
+          [(eq? m '+tup) +tup]
+          [(eq? m '-tup) -tup]
+          [(eq? m '*tup) *tup]
+          [(eq? m 'a*tup) a*tup]
+          [else
+           (error "Operation not recognized!" op)]))
+  (dispatch m))
 
-(define ((component . args) tup)
-  (if (null? args)
-      tup
-      ((apply component (cdr args)) (ref tup (car args)))))
-
-
-(define ((arithmetic-tup-factory op) tup1 tup2)
-  (if (and (tagged-list? tup1) (tagged-list? tup2)
-           (and (eq? (length tup1) (length tup2))))
-      ((define (op-lists l1 l2 op)
-         (if (or (null? l1))
-             null
-             (cons (list op (car l1) (car l2)) (op-lists (cdr l1) (cdr l2) op))))
-       (define (mul-lists l1 l2)
-         (cons '+ (op-lists l1 l2 '*)))
-       (let ((tag1 (get-tag tup1))
-             (tag2 (get-tag tup2))
-             (rest1 (cdr tup1))
-             (rest2 (cdr tup2)))
-         (cond [(and (eq? tag1 'up) (eq? tag2 'up) (not (eq? op '*)))
-                (apply up (op-lists rest1 rest2 op))]
-               [(and (eq? tag1 'down) (eq? tag2 'down) (not (eq? op '*)))
-                (apply down (op-lists rest1 rest2 op))]
-               [(or (and (eq? tag1 'down) (eq? tag2 'up) (eq? op '*))
-                    (and (eq? tag1 'up) (eq? tag2 'down) (eq? op '*)))
-                (mul-lists rest1 rest2)]
-               [else
-                (error "Unrecognized operation on tuples of the given type!" op tag1 tag2)])))
-      
-      (error "Tuples must be tagged up or down, each, and be of equal length!")))
-
-(define add-tup
-  (arithmetic-tup-factory '+))
-(define sub-tup
-  (arithmetic-tup-factory '-))
-(define mul-tup
-  (arithmetic-tup-factory '*))
+(define up
+  (install-tuples-package 'up))
+(define down
+  (install-tuples-package 'down))
+(define ref
+  (install-tuples-package 'ref))
+(define component
+  (install-tuples-package 'component))
+(define +tup
+  (install-tuples-package '+tup))
+(define -tup
+  (install-tuples-package '-tup))
+(define *tup
+  (install-tuples-package '*tup))
+(define a*tup
+  (install-tuples-package 'a*tup))
 
 
 (define v
   (down 'v_1 'v_2 'v_3))
 (define w
-  (down 'w_1 'w_2 'w_3))
+  (up 'w^1 'w^2 'w^3))
 
 
 ;;(define ((L-free-particle1 mass) local)
